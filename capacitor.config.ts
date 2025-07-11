e6fe742b35a1865242898b71492e8a388f5de504
riddhikusal <riddhikusal.datta@gmail.com>
2025-07-11 23:23:06 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IonicReactBlankProjectSkeleton',
+  webDir: 'dist'
+};
+
+export default config;
