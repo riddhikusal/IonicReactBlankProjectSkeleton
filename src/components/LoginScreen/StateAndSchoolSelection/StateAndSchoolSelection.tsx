fddf60ea44eacf872ca1ddfc824f7dea92f7e4f3
Arnab Dutta <116145209+arnabduttamcc@users.noreply.github.com>
2025-08-24 16:26:23 +0530

    new code add

--- src/components/LoginScreen/StateAndSchoolSelection/StateAndSchoolSelection.tsx
+++ src/components/LoginScreen/StateAndSchoolSelection/StateAndSchoolSelection.tsx
@@ -3,22 +3,29 @@ import React from 'react';
 import '../LoginScreen.css';
 import PadaiButton from '../../Common/Buttons/Button';
 import { useHistory } from 'react-router-dom';
+import { LoginForm } from '../../../pages/LoginScreen/LoginScreen.interface';
 
-const PadAIStateAndSchoolSelection = ({ setStep }: { setStep: (step: number) => void }) => {
+interface StateAndSchoolSelectionProps {
+    setStep: (step: number) => void;
+    loginForm: LoginForm;
+    setLoginForm: (loginForm: LoginForm) => void;
+}
+
+const PadAIStateAndSchoolSelection = ({ setStep, loginForm, setLoginForm }: StateAndSchoolSelectionProps) => {
     const history = useHistory();
     return (
         <div className='padAILogin-container'>
             <IonCard className='padAILoginCard'>
                 <IonCardContent>
-                    <IonText >
+                    <IonText className='ion-margin-bottom'>
                         <p className='padAILoginTitle'>Sign Up</p>
                         <p className='padAILoginSubTitle'>Select your state and school</p>
                     </IonText>
                     <IonText className='ion-margin-top'>
-                        <p className='padAILoginSubTitle'>State</p>
+                        <p className='padAIInputLabel'>State</p>
                     </IonText>
-                    <IonItem>
-                        <IonSelect aria-label="state" interface="modal" placeholder="Select state">
+                    <IonItem className='padAIInput'>
+                        <IonSelect aria-label="state" interface="modal" placeholder="Select state" label-placement="stacked" value={loginForm.login_screen_state_and_school_selection.state} onIonChange={(e) => setLoginForm({ ...loginForm, login_screen_state_and_school_selection: { ...loginForm.login_screen_state_and_school_selection, state: e.detail.value || '' } })}>
                             <IonSelectOption value="Delhi">Delhi</IonSelectOption>
                             <IonSelectOption value="Maharashtra">Maharashtra</IonSelectOption>
                             <IonSelectOption value="Tamil Nadu">Tamil Nadu</IonSelectOption>
@@ -31,10 +38,10 @@ const PadAIStateAndSchoolSelection = ({ setStep }: { setStep: (step: number) => 
                         </IonSelect>
                     </IonItem>
                     <IonText className='ion-margin-top'>
-                        <p className='padAILoginSubTitle'>School</p>
+                        <p className='padAIInputLabel'>School</p>
                     </IonText>
-                    <IonItem>
-                        <IonSelect aria-label="school" interface="modal" placeholder="Select school" >
+                    <IonItem className='padAIInput'>
+                        <IonSelect aria-label="school" interface="modal" placeholder="Select school" label-placement="stacked" value={loginForm.login_screen_state_and_school_selection.school} onIonChange={(e) => setLoginForm({ ...loginForm, login_screen_state_and_school_selection: { ...loginForm.login_screen_state_and_school_selection, school: e.detail.value || '' } })}>
                             <IonSelectOption value="School 1">School 1</IonSelectOption>
                             <IonSelectOption value="School 2">School 2</IonSelectOption>
                             <IonSelectOption value="School 3">School 3</IonSelectOption>
--- src/pages/LoginScreen/LoginScreen.tsx
+++ src/pages/LoginScreen/LoginScreen.tsx
@@ -4,19 +4,23 @@ import PadaiHeader from '../../components/LandingScreen/Header/Header';
 import PadaiHeaderBanner from '../../components/LandingScreen/HeaderBanner/HeaderBanner';
 import PadaiFooter from '../../components/LandingScreen/Footer/Footer';
 import PadAIStateAndSchoolSelection from '../../components/LoginScreen/StateAndSchoolSelection/StateAndSchoolSelection';
+import PadAIPhoneNoValidation from '../../components/LoginScreen/PhoneNoValidation/PhoneNoValidation';
+import { LoginForm, defaultLoginForm } from './LoginScreen.interface';
 
 const PadAILoginScreen = () => {
     const [step, setStep] = useState(1);
+    const [loginForm, setLoginForm] = useState<LoginForm>(JSON.parse(JSON.stringify(defaultLoginForm)));
     return (
         <IonPage>
         <PadaiHeader />
         <IonContent>
           <PadaiHeaderBanner />
-          {step === 1 && <PadAIStateAndSchoolSelection setStep={setStep} />}
+          {step === 1 && <PadAIStateAndSchoolSelection setStep={setStep} loginForm={loginForm} setLoginForm={setLoginForm} />}
+          {step === 2 && <PadAIPhoneNoValidation setStep={setStep} loginForm={loginForm} setLoginForm={setLoginForm} />}
           <PadaiFooter />
         </IonContent>
       </IonPage>
     );
 };
 
-export default PadAILoginScreen;
+export default PadAILoginScreen;  
