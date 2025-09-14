4070186783cd083a30b25dbe91bc43973d0a7276
Arnab Dutta <116145209+arnabduttamcc@users.noreply.github.com>
2025-09-14 09:17:22 +0530

    cat panel updated

--- src/components/ContentView/ContentAIPanel/ContentAIPanel.tsx
+++ src/components/ContentView/ContentAIPanel/ContentAIPanel.tsx
@@ -1,31 +1,56 @@
-import { IonButton, IonButtons, IonChip, IonCol, IonIcon, IonImg, IonRow, IonText, IonTextarea } from '@ionic/react';
+import { IonButton, IonButtons, IonChip, IonCol, IonContent, IonIcon, IonImg, IonModal, IonRow, IonText, IonTextarea } from '@ionic/react';
 import './ContentAIPanel.css';
-import { mic, play, playForward, searchOutline } from 'ionicons/icons';
+import { ellipsisVertical, mic, play, playForward, searchOutline } from 'ionicons/icons';
+import { useRef, useState } from 'react';
 const PadAIContentAIPanel = () => {
-    return (
-        <div className="padAIcontentAIPanel-container">
-            <IonRow>
-                <IonCol size='3'>
-                    <IonButton className="btnGradient" fill='clear'>
-                        <IonImg src={'/assets/images/contentScreens/aiAsk.png'} alt='ask' />
-                        <IonText>Ask</IonText>
-                    </IonButton>
-                </IonCol>
-                <IonCol size='6'>
-                    <IonTextarea 
-                        placeholder='Ask anything about the video'
-                    ></IonTextarea>
-                </IonCol>
-                <IonCol size='3'>
-                    <IonButtons>
-                        {/* <IonButton fill='clear' > <IonIcon icon={searchOutline} className='footericons'></IonIcon></IonButton> */}
-                        <IonButton fill='clear' > <IonIcon icon={play} className='footericons' ></IonIcon></IonButton>
-                        <IonButton fill='clear' > <IonIcon icon={mic} className='footericons'></IonIcon></IonButton>
-                    </IonButtons>
-                </IonCol>
-            </IonRow>
+    const [showSelectedText, setShowSelectedText] = useState<boolean>(false);
+    const modal = useRef<HTMLIonModalElement>(null);
+    return (<>
+        <div className='padAIcontentAIPanel-container-overlay'>
+            {showSelectedText && <div className='padAIcontentAIPanel-description-container'>
+                <IonText className='padAIcontentAIPanel-description-text'><p>
+                    " Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui laudantium perferendis magni at illum, earum itaque quaerat impedit soluta, optio placeat! Ab ipsam earum ex sit praesentium vitae! Repellat saepe commodi laudantium atque, praesentium aliquam expedita corrupti necessitatibus eaque possimus."
+                </p></IonText>
+            </div>}
+            <div className="padAIcontentAIPanel-container">
+                <IonRow>
+                    <IonCol size='3'>
+                        <IonButton  className="btnGradient" fill='clear' onClick={() => { setShowSelectedText(!showSelectedText) }}>
+                            <IonImg src={'/assets/images/contentScreens/aiAsk.png'} alt='ask' />
+                            <IonText>Ask</IonText>
+                        </IonButton>
+                    </IonCol>
+                    <IonCol size='6'>
+                        <IonTextarea
+                            placeholder='Ask anything ..'
+                        ></IonTextarea>
+                    </IonCol>
+                    <IonCol size='3'>
+                        <IonButtons>
+                            <IonButton fill='clear' > <IonIcon icon={play} className='footericons' ></IonIcon></IonButton>
+                            <IonButton fill='clear' > <IonIcon icon={mic} className='footericons'></IonIcon></IonButton>
+                            <IonButton fill='clear' > <IonIcon icon={ellipsisVertical} className='footericons'></IonIcon></IonButton>
+                        </IonButtons>
+                    </IonCol>
+                </IonRow>
+            </div>
         </div>
+        <IonModal 
+            ref={modal} 
+            trigger="open-modal" 
+            initialBreakpoint={0.25} 
+            breakpoints={[0, 0.25, 0.5, 0.75]}
+            style={{ '--z-index': '999' }}
+        >
+            <IonContent className="ion-padding">
+
+            </IonContent>
+        </IonModal>
+    </>
+
     )
 }
 
 export default PadAIContentAIPanel;
+
+// id="open-modal"
--- src/pages/ContentViewScreens/dummyData.ts
+++ src/pages/ContentViewScreens/dummyData.ts
@@ -2,7 +2,7 @@ export const VideoContentDummy = {
     id: 'videoContentDummyID',
     title: 'videoContentDummy',
     url: 'https://d1rb72t9cnnyis.cloudfront.net/CBSE/X/Science/CH12/AI-Video-Eng/Magnetic_Effects_of_Electric_Current_English_Playlist.m3u8',
-    thumbnail: "",
+    thumbnail: "https://d1rb72t9cnnyis.cloudfront.net/CBSE/X/Science/CH12/Poster-Eng-Ch12.png",
     duration: "",
     description: "",
 }
